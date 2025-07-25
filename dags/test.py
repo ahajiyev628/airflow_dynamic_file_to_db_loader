33214656371baf4651247483f75d7abfd4b420f9
ahajiyev628 <66174474+ahajiyev628@users.noreply.github.com>
2025-07-25 22:07:37 +0400

    Update test.py

--- dags/test.py
+++ dags/test.py
@@ -1,1 +1,32 @@
+from airflow import DAG
+from datetime import datetime
+from airflow.operators.python import PythonOperator
+from airflow.operators.dummy import DummyOperator
 
+default_args = {
+  "owner": "airflow"
+}
+
+def hello_world():
+  print("Hello world!")
+
+def hello_airflow():
+  print("Hellow airflow!")
+
+with DAG (
+  dag_id = "test_dag",
+  description = "Demo dag",
+  scheduler_interval = "5 0 * * *",
+  default_args = default_args,
+  start_date = datetime(2025,7,7)
+) as dag:
+  start_task = DummyOperator("start_task")
+  end_task = DummyOperator("end_task")
+
+  hello_world_task = PythonOperator("hello_world_task",
+                                     python_collable = hello_world)
+
+  hello_airflow_task = PythonOperator("hello_airflow_task",
+                                     python_collable = hello_airflow)
+
+  start_task >> hello_world_task >> hello_airflow_task >> end_task
