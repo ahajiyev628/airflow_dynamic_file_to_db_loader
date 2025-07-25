79ace1447dacd6aa4e3b6544ddfa2a55c3e36643
ahajiyev628 <66174474+ahajiyev628@users.noreply.github.com>
2025-07-25 22:12:45 +0400

    Update test.py

--- dags/test.py
+++ dags/test.py
@@ -20,13 +20,13 @@ with DAG (
   default_args = default_args,
   start_date = datetime(2025,7,7)
 ) as dag:
-  start_task = DummyOperator("start_task")
-  end_task = DummyOperator("end_task")
+  start_task = DummyOperator(task_id="start_task")
+  end_task = DummyOperator(task_id="end_task")
 
-  hello_world_task = PythonOperator("hello_world_task",
+  hello_world_task = PythonOperator(task_id="hello_world_task",
                                      python_collable = hello_world)
 
-  hello_airflow_task = PythonOperator("hello_airflow_task",
+  hello_airflow_task = PythonOperator(task_id="hello_airflow_task",
                                      python_collable = hello_airflow)
 
   start_task >> hello_world_task >> hello_airflow_task >> end_task
